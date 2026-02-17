b25d2d23db93b01885b631912a0fd6ce8b5031de
Pool Golez <loop.edward@gmail.com>
2026-02-17 16:58:48 +0800

    Fix CORS issue

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  const origin = configService.get<string>('FRONTEND_URL');
+  console.log({ FRONTEND_URL: origin });
+
+  app.enableCors({
+    origin,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 8080);
 }
 void bootstrap();
